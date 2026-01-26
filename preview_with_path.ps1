0b824bd010baec88ffa4ef084a3fdcb4ff53198c
alialbeladi <47190890+alialbeladi@users.noreply.github.com>
2026-01-26 08:41:06 +0300

    updates

---
+++ preview_with_path.ps1
@@ -0,0 +1,2 @@
+$env:PATH += ";C:\Users\thinkcenter\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\Scripts"
+& "C:\Users\thinkcenter\AppData\Local\Programs\Quarto\bin\quarto.exe" preview
